a109818b5042c650d9acf794b6774026aa9ca157
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2024-03-28 00:35:26 +0300

    Add files via upload

---
+++ Store/StoreaApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "hello raotung";
+        //}
+    }
+}
+                                        
